e227c0e32405f6dcc5da7188218d5f7ff63904ec
debsfraga <deborafragamoreira@gmail.com>
2026-03-02 22:05:10 -0300

    teste de texto e imagem

--- App.tsx
+++ App.tsx
@@ -1,11 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image} from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text style={styles.texto}>Hello World! No APP</Text>
       <StatusBar style="inverted" />
+      <Image source={require('./assets/picapau.jpg')} style={styles.logo} /*faz com que a imagem caiba exatamente no espço determinado*/ resizeMode='contain'/>
+      <Text>Pica-Pau é um personagem da série estadunidense {'\n'}de mesmo nome, um pica-pau antropomórfico, que {'\n'}estrelou vários curta-metragens de animação produzidos{'\n'} pelo estúdio de Walter Lantz e distribuídos pela{'\n'} Universal Pictures.</Text>
     </View>
   );
 }
@@ -17,8 +18,8 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
-  texto: {
-    fontSize: 32,
-    fontFamily: 'cursive',
+  logo: {
+    width: 250,
+    height: 250,
   }
 });
