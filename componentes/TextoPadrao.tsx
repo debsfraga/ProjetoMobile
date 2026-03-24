ce06b01fcb50a9139ce9220c78c6ff3e6a6a448d
debsfraga <deborafragamoreira@gmail.com>
2026-03-23 22:24:09 -0300

    Cria pasta componentes

--- componentes/TextoPadrao.tsx
+++ componentes/TextoPadrao.tsx
@@ -1,6 +1,6 @@
 import { Text, StyleSheet } from "react-native";
 
-export default function TextoPadrao({children, estiloEspecifico/**/}: any) {
+export default function TextoPadrao({children, estiloEspecifico}: any) {
     return <Text style={[estilos.padrao, estiloEspecifico]}>{children}</Text>
 }
 
