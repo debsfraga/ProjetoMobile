f0a22a510dad3f76215f6f6b06d5cf9ecb8c5c36
debsfraga <deborafragamoreira@gmail.com>
2026-04-27 22:03:53 -0300

    criar cards

---
+++ telas/mocks/listaProduto.tsx
@@ -0,0 +1,21 @@
+const lista_produtos = {
+    itens: {
+        titulo: "Veja nossos produtos!",
+        lista: [
+            {
+                id: 1,
+                nome: "Livro",
+                descricao: "4pcs. Temos outros modelos e cores.",
+                imagem: require('../../assets/logo.png'),
+            },
+            {
+                id: 2,
+                nome: "Livro2",
+                descricao: "4pcs. Temos outros modelos e cores.",
+                imagem: require('../../assets/logo.png'),
+            },
+        ]
+    }
+}
+
+export default lista_produtos;
